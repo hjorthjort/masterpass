bfede3ec9e37851b08921e04aa0fbc38dddf79b8
hjorthjort <rikard.hjort@gmail.com>
2016-12-03 19:03:20 +0100

    Clean up and clarify the Flags module

--- flags.hs
+++ flags.hs
@@ -1,3 +1,7 @@
+-- | Simple module for handling command line flags, where each flag starts
+-- | with an arbitrary number of hyphens, followed by a word, and optionally an
+-- | equals sign and an argument.
+-- | Example: -a, --add, --file=myfile.txt
 module Flags(
     getFlags,
     isSet,
@@ -9,10 +13,13 @@ module Flags(
 import Data.Map as Map
 import Data.Maybe
 
+-- | A flag, such as -f or --no-edit
 type Flag = String
 type FlagArg = String
 type FlagList = Map Flag (Maybe FlagArg)
 
+-- | Takes a list of command line arguments and converts them to a dictionery of
+-- | flags and their values.
 getFlags :: [String] -> FlagList
 getFlags = fromList . Prelude.map (\flag -> (parseFlag flag, if getFlagArg flag == []
                                    then Nothing
@@ -26,6 +33,7 @@ getFlags = fromList . Prelude.map (\flag -> (parseFlag flag, if getFlagArg flag 
         parseFlag = takeWhile (/='=') . dropWhile (=='-')
         getFlagArg = drop 1 . dropWhile (/='=')
 
+-- | Checks wether a flag exists in a given flag dictionary.
 isSet :: Flag -> FlagList -> Bool
 isSet f fs
   | isNothing (Map.lookup f fs) = False
@@ -36,12 +44,13 @@ getFlagArgSafe :: Flag -> FlagList -> Maybe FlagArg
 getFlagArgSafe f fs | isSet f fs =  fromJust $ Map.lookup f fs
                 | otherwise = Nothing
 
+-- | Get the argument for a flag. Will crash if the flag doesn't exist.
+getFlagArg :: Flag -> FlagList -> FlagArg
+getFlagArg f fs = fromJust $ getFlagArgSafe f fs
+
 -- | If the requested flag exists, returns its argument, otherwise the
--- fallbakc value.
+-- | fallback value.
 maybeFlags :: FlagArg -> Flag -> FlagList -> FlagArg
 maybeFlags fallback flag flags = if isSet flag flags
                                     then getFlagArg flag flags
                                     else fallback
-
-getFlagArg :: Flag -> FlagList -> FlagArg
-getFlagArg f fs = fromJust $ getFlagArgSafe f fs
