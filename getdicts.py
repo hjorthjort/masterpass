2a9a142442efd9846db0bea6ba4f3b13b3545578
hjorthjort <rikard.hjort@gmail.com>
2018-04-06 17:12:12 +0900

    Download more dicitonaries

--- getdicts.py
+++ getdicts.py
@@ -1,21 +1,27 @@
 import urllib3, re
+from urllib.parse import quote
 
 http = urllib3.PoolManager()
 
 # I want to avoid special characters.
 oddChars = re.compile(r'[^A-Za-z0-9]')
 
-# Swedish
+# Swedish, English and German
 
-response = http.request('GET', 
-        'https://raw.githubusercontent.com/titoBouzout/Dictionaries/master/Swedish.dic')
-responseString = str(response.data, 'utf8')
-lines = [ line.split('/')[0] for line in responseString.split('\n') ]
+for lang in ["Swedish", "English (American)", "German"]:
+    url = 'https://raw.githubusercontent.com/titoBouzout/Dictionaries/master/%s.dic' % quote(lang)
+    response = http.request('GET', url)
+    print('Fetching ' + url)
+    responseString = str(response.data, 'utf8')
+    # All lines in these collection end in "/<some letter>". This splits the
+    # string into lines, while also getting rid of this ending.
+    lines = [ line.split('/')[0] for line in responseString.split('\n') ]
+    # First line contains number of rows.
+    filtered = lines[1:]
+    # Only allow words with characters A-Z, a-z and 0-9.
+    filtered = [ line for line in filtered if oddChars.search(line) is None ]
+    # Only allow words of 5 characters or more.
+    filtered = [ line for line in filtered if len(line) >= 5 ]
 
-# Only allow words with characters A-Z, a-z and 0-9.
-filtered = [ line for line in lines if oddChars.search(line) is None ]
-# Only allow words of 5 characters or more.
-filtered = [ line for line in filtered if len(line) >= 5 ]
-
-with open('dict/swedish', 'w') as f:
-    print("\n".join(filtered), file=f)
+    with open('dict/%s' % lang.lower(), 'w') as f:
+        print("\n".join(filtered), file=f)
